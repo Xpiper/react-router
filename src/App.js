5c05b0886bf6976bd8d1974a25fce0b3582014ad
apiper <apiper@126.com>
2022-01-10 22:48:03 +0800

    add <Link> component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/About' element={<About />} />
-    </Routes >
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes >
+    </>
   );
 }
 
