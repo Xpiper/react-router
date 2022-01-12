614de10296e049d79566384865989b709ebf88ab
apiper <apiper@126.com>
2022-01-12 23:56:07 +0800

    Navigating Programmatically & useNavigate hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 export const Home = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            Home page
-        </div>
+        <>
+            <div>Home page</div>
+            <button onClick={() => navigate('order-summary', { replace: true })}>Place order</button>
+        </>
     )
 }
