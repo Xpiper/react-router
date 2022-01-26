922b0bfa0dd78842ce28b0d4ab15bc5b21fb3651
apiper <apiper@126.com>
2022-01-26 21:17:37 +0800

    Authentication and Protected Routes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ import { NewProducts } from './components/NewProducts';
 import { Users } from './components/Users';
 import { UserDetail } from './components/UserDetail';
 import React from 'react';
+import { Profile } from './components/Profile';
+import { AuthProvider } from './components/auth';
+import { Login } from './components/Login';
+import { RequireAuth } from './components/RequireAuth';
 const LazyAbout = react.lazy(() => import('./components/About'))
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
@@ -36,8 +40,10 @@ function App() {
         </Route>
         <Route path='/users/:userId' element={<UserDetail></UserDetail>}></Route>
         <Route path='*' element={<NoMatch />} />
+        <Route path='/profile' element={<RequireAuth><Profile /></RequireAuth>}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes >
-    </>
+    </AuthProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from './auth'
 
 export const Navbar = () => {
 
@@ -10,11 +11,19 @@ export const Navbar = () => {
         }
     }
 
+    const auth = useAuth();
+
     return (
         <nav className='primary-nav'>
             <NavLink style={navLinkStyles} to='/'>Home</NavLink>
             <NavLink style={navLinkStyles} to='/about'>About</NavLink>
             <NavLink style={navLinkStyles} to='/products'>Produts</NavLink>
+            <NavLink style={navLinkStyles} to='/profile'>Profile</NavLink>
+            {
+                !auth.user && (
+                    <NavLink style={navLinkStyles} to='/login'>login</NavLink>
+                )
+            }
         </nav>
     )
 }
